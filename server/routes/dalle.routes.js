3592638cbadba22472f4aa25a2a78bb187b2ee85
AliJiuJiu <xiaopangengine@hotmail.com>
2023-05-25 08:06:13 +0100

    final project

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -13,7 +13,7 @@ const config = new Configuration({
 const openai = new OpenAIApi(config);
 
 router.route("/").get((req, res) => {
-  res.status(200).json({ message: "Hello from DALL.E" });
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" });
 });
 
 router.route("/").post(async (req, res) => {
@@ -21,12 +21,12 @@ router.route("/").post(async (req, res) => {
     const { prompt } = req.body;
 
     const response = await openai.createImage({
-      prompt: prompt,
+      prompt,
       n: 1,
       size: "1024x1024",
       response_format: "b64_json",
     });
-    console.log(response.data.data[0].b64_json);
+
     const image = response.data.data[0].b64_json;
 
     res.status(200).json({ photo: image });
